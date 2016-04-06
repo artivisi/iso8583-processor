7dd22484b43667110ef3007238aaa0f831c73115
Adi Sulistiono <sulistiono.adi@gmail.com>
2016-04-06 13:34:29 +0700

    fixing sql flywaydb untuk database postgre

--- iso8583-processor-persistence/src/main/resources/db/migration/V0_0_0_1__Schema_ISO8583_1_0.sql
+++ iso8583-processor-persistence/src/main/resources/db/migration/V0_0_0_1__Schema_ISO8583_1_0.sql
@@ -25,42 +25,55 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-CREATE TABLE `iso8583_mapper` (
-  `id` varchar(36) NOT NULL,
-  `name` varchar(35) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_mapper_name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- TABLE iso8583_mapper
+CREATE TABLE iso8583_mapper (
+  id character varying(36) NOT NULL,
+  name character varying(35) NOT NULL,
+  description character varying(255) DEFAULT NULL
+);
+ALTER TABLE ONLY iso8583_mapper
+    ADD CONSTRAINT iso8583_mapper_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY iso8583_mapper
+    ADD CONSTRAINT unique_iso8583_mapper UNIQUE (name);
 
-CREATE TABLE `iso8583_dataelement` (
-  `id` varchar(36) NOT NULL,
-  `id_mapper` varchar(36) NOT NULL,
-  `dataelement_number` int(11) NOT NULL,
-  `dataelement_name` varchar(50) NOT NULL,
-  `dataelement_type` varchar(12) NOT NULL,
-  `dataelement_length_type` varchar(8) NOT NULL,
-  `dataelement_length` int(11) DEFAULT NULL,
-  `dataelement_length_prefix` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`,`dataelement_number`),
-  UNIQUE KEY `unique_mapper_number` (`id_mapper`,`dataelement_number`),
-  UNIQUE KEY `unique_mapper_name` (`id_mapper`,`dataelement_name`),
-  CONSTRAINT `fk_dataelement_mapper` FOREIGN KEY (`id_mapper`) REFERENCES `iso8583_mapper` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- TABLE iso8583_dataelement
+CREATE TABLE iso8583_dataelement (
+  id character varying(36) NOT NULL,
+  id_mapper character varying(36) NOT NULL,
+  dataelement_number integer NOT NULL,
+  dataelement_name character varying(50) NOT NULL,
+  dataelement_type character varying(12) NOT NULL,
+  dataelement_length_type character varying(8) NOT NULL,
+  dataelement_length integer DEFAULT NULL,
+  dataelement_length_prefix integer DEFAULT NULL
+);
+ALTER TABLE ONLY iso8583_dataelement
+    ADD CONSTRAINT iso8583_dataelement_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY iso8583_dataelement
+    ADD CONSTRAINT unique_mapper_number UNIQUE (id_mapper,dataelement_number);
+ALTER TABLE ONLY iso8583_dataelement
+    ADD CONSTRAINT unique_mapper_name UNIQUE (id_mapper,dataelement_name);
+ALTER TABLE ONLY iso8583_dataelement
+    ADD CONSTRAINT fk_dataelement_mapper FOREIGN KEY (id_mapper) REFERENCES iso8583_mapper (id);
 
-CREATE TABLE `iso8583_subelement` (
-  `id` varchar(36) NOT NULL,
-  `id_data_element` varchar(36) NOT NULL,
-  `subelement_number` int(11) NOT NULL,
-  `subelement_type` varchar(12) NOT NULL,
-  `subelement_name` varchar(50) NOT NULL,
-  `subelement_length` int(11) NOT NULL,
-  `subelement_padding` varchar(1) DEFAULT NULL,
-  `subelement_padding_pos` varchar(5) DEFAULT NULL,
-  `subelement_separator` varchar(1) DEFAULT NULL,
-  `subelement_repeated` bit(1) NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (`id`,`subelement_number`),
-  UNIQUE KEY `unique_element_number` (`id_data_element`,`subelement_number`),
-  UNIQUE KEY `unique_element_name` (`id_data_element`,`subelement_name`),
-  CONSTRAINT `fk_subelement_dataelement` FOREIGN KEY (`id_data_element`) REFERENCES `iso8583_dataelement` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- TABLE iso8583_subelement
+CREATE TABLE iso8583_subelement (
+  id character varying(36) NOT NULL,
+  id_data_element character varying(36) NOT NULL,
+  subelement_number integer NOT NULL,
+  subelement_type character varying(12) NOT NULL,
+  subelement_name character varying(50) NOT NULL,
+  subelement_length integer NOT NULL,
+  subelement_padding character varying(1) DEFAULT NULL,
+  subelement_padding_pos character varying(5) DEFAULT NULL,
+  subelement_separator character varying(1) DEFAULT NULL,
+  subelement_repeated boolean NOT NULL
+);
+ALTER TABLE ONLY iso8583_subelement
+    ADD CONSTRAINT iso8583_subelement_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY iso8583_subelement
+    ADD CONSTRAINT unique_element_number UNIQUE (id_data_element,subelement_number);
+ALTER TABLE ONLY iso8583_subelement
+    ADD CONSTRAINT unique_element_name UNIQUE (id_data_element,subelement_name);
+ALTER TABLE ONLY iso8583_subelement
+    ADD CONSTRAINT fk_subelement_dataelement FOREIGN KEY (id_data_element) REFERENCES iso8583_dataelement (id);
